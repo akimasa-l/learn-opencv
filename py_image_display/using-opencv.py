c8c005189263da37dd743521250a9e49606a9f09
akimasa-l <liumingzheng8@outlook.com>
2021-05-29 21:03:05 +0900

    これがopencvのパワー！！！！！！！！

--- py_image_display/using-opencv.py
+++ py_image_display/using-opencv.py
@@ -1,5 +1,6 @@
 import cv2
 img = cv2.imread("./opencv-logo-white.png")
+cv2.namedWindow("img",cv2.WINDOW_NORMAL)#WINDOW_NORMALにするとウィンドウサイズが変わる
 cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
